28ab5c91085fce27e1c8f4a8f518b0e122cb12ee
adelhighrullin <adelhighrullin@gmail.com>
2020-08-05 14:22:27 +0300

    little updates

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import About from './About';
 import Appointment from './Appointment';
-import Header from './Header';
+import Done from './Done';
 import Footer from './Footer';
+import Header from './Header';
 import Home from './Home';
 import NNPage from './NNPage';
+import TestDone from './TestDone';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 function App() {
   return (
@@ -14,8 +16,10 @@ function App() {
         <Switch>
           <Route path="/" exact component={ () => <Home /> } />
           <Route path="/About" exact component={ () => <About /> } />
-          <Route path="/NNPage" exact component={ () => <NNPage /> } />
           <Route path="/Appointment" exact component={ () => <Appointment /> } />
+          <Route path="/Done" exact component={ () => <Done /> } />
+          <Route path="/NNPage" exact component={ () => <NNPage /> } />
+          <Route path="/TestDone" exact component={ () => <TestDone /> } />
         </Switch>
         <Footer />
       </Router>
--- src/Header.js
+++ src/Header.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import StickyHeader from 'react-sticky-header';
 
 function Header() {
   return (
     <div className="header">
-      <img src="https://cdn1.flamp.ru/dc648c46e17076daf4310fc5d0e967c7.png" />
+      <Link to="/">
+        <img src="https://cdn1.flamp.ru/dc648c46e17076daf4310fc5d0e967c7.png" />
+      </Link>
       <div>
         <Link to="/About">О нас</Link>
       </div>
