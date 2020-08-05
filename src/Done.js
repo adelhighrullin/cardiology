e226764b3fb09cc79572841f02293c0d8fe37de6
adelhighrullin <adelhighrullin@gmail.com>
2020-08-05 11:45:16 +0300

    added two pages

---
+++ src/Done.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+
+function Done() {
+    return (
+        <div>
+            <p>Запись на прием осуществлена.</p>
+        </div>
+    )
+}
+
+export default Done;
