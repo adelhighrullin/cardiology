50a52e1775a4ec4d40e868521949e5f5c89ac904
adelhighrullin <adelhighrullin@gmail.com>
2020-08-14 21:04:05 +0300

    added yandex map to About page

--- src/About.js
+++ src/About.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { YMaps, Map, Placemark, FullscreenControl, ZoomControl } from 'react-yandex-maps';
 
 function About() {
   return (
     <div className="column">
       <p>О нас........</p>
+      <YMaps>
+        <div id="map">
+          
+          <Map defaultState={{ center: [55.748579, 49.224803], zoom: 16 }}>
+            <FullscreenControl />
+            <Placemark geometry={[55.748579, 49.224803]} />
+            <ZoomControl options={{ float: 'right' }} />
+            </Map>
+        </div>
+      </YMaps>
       <Link to="/"><button>Вернуться на главную страницу</button></Link>
     </div>
   )
